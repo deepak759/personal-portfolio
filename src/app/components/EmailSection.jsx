40ba240e1880c6592901e8de2f512e38402e284c
Deepak <deepaksharma83407@gmail.com>
2023-12-17 04:17:10 +0530

    fixed error in contact form

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -131,12 +131,12 @@ const handlesChange = (e) => {
                 placeholder="Let's talk about..."
               />
             </div>
-            <Link
+            <a
               href={`mailto:deepaksharma28787@gmail.com?subject=Regarding ${subject}&body=${message}`}
               className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
             >
               Send Message
-            </Link>
+            </a>
           </div>
         )}
       </div>
