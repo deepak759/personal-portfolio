82024e28636df080cb5e5620fe7b0ff4cf94a7d0
deepak759 <83724070+deepak759@users.noreply.github.com>
2026-03-17 17:21:29 +0530

    Update LeetCode icon import path

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
-import LeetCodeIcon from "../../../public/linkedin-icon.svg";
+import LeetCodeIcon from "../../../public/leetcode.svg";
 import Link from "next/link";
 import Image from "next/image";
 
